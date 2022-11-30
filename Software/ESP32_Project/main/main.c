5b142185c98bd00bdd5f00aa74fb6410aabaa6bd
letian <1656733965@qq.com>
2022-11-30 22:27:04 +0800

    "²âÊÔÖÐÎÄ"

--- Software/ESP32_Project/main/main.c
+++ Software/ESP32_Project/main/main.c
@@ -2,8 +2,8 @@
  * @Author: letian
  * @Date: 2022-11-29 14:19
  * @LastEditors: letian
- * @LastEditTime: 2022-11-30 16:51
- * @FilePath: \project\main\main.c
+ * @LastEditTime: 2022-11-30 22:24
+ * @FilePath: \ESP32_Project\main\main.c
  * @Description:
  * Copyright (c) 2022 by letian 1656733965@qq.com, All Rights Reserved.
  */
